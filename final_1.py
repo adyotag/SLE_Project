c6e0f8d489ec61b3d406f5be03eb12b8f20caadc
adyotag <gupta.adyota@berkeley.edu>
2020-12-14 20:09:09 -0500

    Added Logistic Regression and Corresponding plots

--- final_1.py
+++ final_1.py
@@ -1,5 +1,6 @@
 from helper import ExploreDataset
 from helper import poly, LinearRegressionFit, LinearRegressionEval
+from helper import LogisticRegressionFit, LogisticRegressionEval
 from matplotlib import pyplot as plt
 import numpy as np
 from tqdm import tqdm
@@ -10,12 +11,12 @@ import torch as tc
 GPU = "cuda:0" 
 CPU = "cpu"
 
-#paths, onehotkey, FRUIT_SET, labels =  ExploreDataset()
-
+#paths, onehotkey, categorical, FRUIT_SET, labels =  ExploreDataset(savebackup=True)
 
 print('Loading Backup...')
 images = tc.from_numpy( np.load('/data2/adyotagupta/school/X_30000.npy')  ).float().to(CPU)
 onehotkey = tc.from_numpy( np.load('/data2/adyotagupta/school/Y_ohk.npy')  ).float().to(CPU)
+categorical = tc.from_numpy( np.load('/data2/adyotagupta/school/Y_cat.npy')  ).int().to(CPU)
 index = tc.arange(len(images)).float().to(CPU).unsqueeze(1)
 
 # Shuffle data
@@ -25,6 +26,8 @@ shuffled_index = data[:,0].long()
 
 X = data[:,1:]
 y_ohk = onehotkey[shuffled_index]
+y_cat = categorical[shuffled_index]
+NUM_CATS = len(y_ohk[0]) 
 
 
 # split into training, validation, and test
@@ -40,9 +43,9 @@ def split_dataset(arr, test=0.1, valid=0.1):
 
 X_train, X_valid, X_test = split_dataset(X) 
 y_ohk_train, y_ohk_valid, y_ohk_test = split_dataset(y_ohk) 
+y_cat_train, y_cat_valid, y_cat_test = split_dataset(y_cat) 
 
 
-## Linear Regression
 X2_train = poly(X_train, 2)
 X3_train = poly(X_train, 3)
 X2_valid = poly(X_valid, 2)
@@ -50,6 +53,8 @@ X3_valid = poly(X_valid, 3)
 X2_test = poly(X_test, 2)
 X3_test = poly(X_test, 3)
 
+
+## Linear Regression
 print('Training Linear Regression models...\n')
 
 print('Degree = 1')
@@ -71,9 +76,9 @@ print('Validation = {}, Test = {}'.format(LinearRegressionEval(X3_valid, y_ohk_v
 # plot loss
 plt.figure()
 
-plt.plot(DEG1[1], label='Degree 1, One Hot')
-plt.plot(DEG2[1], label='Degree 2, One Hot')
-plt.plot(DEG3[1], label='Degree 3, One Hot')
+plt.plot(DEG1[1], label='Degree 1')
+plt.plot(DEG2[1], label='Degree 2')
+plt.plot(DEG3[1], label='Degree 3')
 
 plt.title("Loss of Linear Regression Models over Epochs")
 plt.xlabel("Epochs")
@@ -82,14 +87,36 @@ plt.legend()
 plt.savefig("LRLoss.png")
 
 
-
 ## Logistic Regression
+print('\n\nTraining Logistic Regression models...\n')
 
+print('Degree = 1')
+DEG1 = LogisticRegressionFit(X_train, y_cat_train, NUM_CATS)
+print('Train = {}'.format(LogisticRegressionEval(X_train, y_cat_train, DEG1[0]) ))
+print('Validation = {}, Test = {}'.format(LogisticRegressionEval(X_valid, y_cat_valid, DEG1[0]),
+						 LogisticRegressionEval(X_test, y_cat_test, DEG1[0]) ))
 
+print('Degree = 2')
+DEG2 = LogisticRegressionFit(X2_train, y_cat_train, NUM_CATS)
+print('Train = {}'.format(LogisticRegressionEval(X2_train, y_cat_train, DEG2[0]) ))
+print('Validation = {}, Test = {}'.format(LogisticRegressionEval(X2_valid, y_cat_valid, DEG2[0]),
+						 LogisticRegressionEval(X2_test, y_cat_test, DEG2[0]) ))
 
+print('Degree = 3')
+DEG3 = LogisticRegressionFit(X3_train, y_cat_train, NUM_CATS)
+print('Train = {}'.format(LogisticRegressionEval(X3_train, y_cat_train, DEG3[0]) ))
+print('Validation = {}, Test = {}'.format(LogisticRegressionEval(X3_valid, y_cat_valid, DEG3[0]),
+						 LogisticRegressionEval(X3_test, y_cat_test, DEG3[0]) ))
 
+# plot loss
+plt.figure()
 
+plt.plot(DEG1[1], label='Degree 1')
+plt.plot(DEG2[1], label='Degree 2')
+plt.plot(DEG3[1], label='Degree 3')
 
-
-
-
+plt.title("Loss of Logistic Regression Models over Epochs")
+plt.xlabel("Epochs")
+plt.ylabel("Cross Entropy Loss")
+plt.legend()
+plt.savefig("LogRLoss.png")
